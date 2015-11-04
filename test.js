a4f19f8fb6277f690a7a6ce608684c4396f1f1fa
Kyle Mitchell <kyle@kemitchell.com>
2015-11-04 13:31:33 -0800

    Test for non-ASCII printable replacement characters

--- test.js
+++ test.js
@@ -5,6 +5,16 @@ var characters = [ ]
 data.forEach(function(element) {
   var character = element.unicode
   if (characters.indexOf(character) > -1) {
-    throw new Error('Already defined "' + character + '" (x' + character.charCodeAt(0).toString(16)  + ')') }
+    throw new Error(
+      'Already defined "' + character + '" '+
+        '(x' + character.charCodeAt(0).toString(16)  + ')') }
   else {
     characters.push(character) }})
+
+var NON_ASCII_PRINTABLE = /[^\x20-\x7E]/
+
+data.forEach(function(element) {
+  var replacement = element.ascii
+  if (NON_ASCII_PRINTABLE.test(replacement)) {
+    throw new Error(
+      replacement + ' contains a non-ASCII printable character') } })
